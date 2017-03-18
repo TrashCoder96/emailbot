adeccfdb84f60fc2661ce87430fda81984863fcd
TrashCoder96 <kiberaction@yandex.ru>
2017-03-19 02:17:03 +0300

    broken commit

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -1,17 +1,17 @@
 package config;
 
-import controller.ManagementController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import service.EmailSkypeService;
+import rest.ManagementController;
+import service.impl.ManagementService;
 
 /**
  * Created by asus-pc on 04.02.2017.
  */
 
 @Configuration
-@ComponentScan(basePackageClasses = { EmailSkypeService.class, ManagementController.class })
+@ComponentScan(basePackageClasses = { ManagementService.class, ManagementController.class })
 public class WebConfig {
 
     @Bean
